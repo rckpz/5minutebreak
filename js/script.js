861e235128e5a35c79b167be380820a888a97d9b
erick <erickface268@gmail.com>
2024-01-20 21:11:18 +0100

    Finalizado

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const sonidoImg = document.getElementById("sonido-img");
   const fondoAudio = document.getElementById("fondo-audio");
-  let contadorInterval; // Declarar el intervalo fuera de la función para que sea accesible en otras funciones
-  let animacionInterval; // Declarar el intervalo para la animación
+  let contadorInterval; // Intervalo del contador
+  let animacionInterval; // Intervalo para la animación
 
   sonidoImg.addEventListener("click", () => {
+    // Control de reproducción de sonido
     if (fondoAudio.paused) {
       fondoAudio.play();
+      fondoAudio.volume = 0.5; // Ajustar volumen al 50%
       sonidoImg.src = "img/smartphone/Sonidoa.svg";
     } else {
       fondoAudio.pause();
@@ -39,11 +41,13 @@ document.addEventListener("DOMContentLoaded", () => {
   playBtn.addEventListener("click", iniciarCuentaAtras);
 
   function iniciarCuentaAtras() {
+    // Iniciar la cuenta atrás antes de la sesión
     playBtn.style.display = "none";
     let cuentaRegresiva = 3;
 
     const cuentaAtrasInterval = setInterval(() => {
       if (cuentaRegresiva > 0) {
+        // Actualizar visualización de la cuenta atrás
         const porcentaje = cuentaRegresiva * 25;
         circuloTurquesa.innerHTML = `<span class="numero">${cuentaRegresiva}</span>`;
         circuloTurquesa.style.width = `${porcentaje}%`;
@@ -62,16 +66,18 @@ document.addEventListener("DOMContentLoaded", () => {
         clearInterval(cuentaAtrasInterval);
         circuloTurquesa.innerHTML = ""; // Limpiar contenido
         iniciarContador();
-        iniciarAnimacionCrecimiento(); // Agregar esta línea para iniciar la animación de crecimiento
+        iniciarAnimacionCrecimiento(); // Iniciar la animación de crecimiento
       }
     }, 1000);
   }
 
   function iniciarContador() {
+    // Iniciar el contador de tiempo durante la sesión
     let tiempoRestante = 5 * 60;
 
     contadorInterval = setInterval(() => {
       if (tiempoRestante > 0) {
+        // Actualizar visualización del contador
         const minutos = Math.floor(tiempoRestante / 60);
         const segundos = tiempoRestante % 60;
         const tiempoFormateado = `${minutos}:${
@@ -89,13 +95,14 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function iniciarAnimacionCrecimiento() {
+    // Iniciar la animación de crecimiento y respiración
     let porcentaje = 25; // Comienza en 25%
-    let fase = "crecimiento"; // Puede ser 'crecimiento', 'espera' o 'decrecimiento'
+    let fase = "crecimiento";
 
     // Crear el elemento de texto y agregarlo al círculo turquesa
     const breathText = document.createElement("span");
     breathText.id = "breath-text";
-    breathText.classList.add("breath-text"); // Agrega una clase para aplicar estilos
+    breathText.classList.add("breath-text");
     circuloTurquesa.appendChild(breathText);
 
     animacionInterval = setInterval(() => {
@@ -104,16 +111,16 @@ document.addEventListener("DOMContentLoaded", () => {
         if (porcentaje < 86) {
           circuloTurquesa.style.width = `${porcentaje}%`;
           circuloTurquesa.style.height = `${porcentaje}%`;
-          porcentaje += 2; // Ajusta la velocidad de crecimiento
+          porcentaje += 2; // Ajustar velocidad de crecimiento
           const fontSize = `calc(${3 + porcentaje / 15}vw)`;
           breathText.style.cssText = `font-size: ${fontSize}; color: var(--color-blanco); transition: font-size 1s ease-in-out;`;
-          breathText.innerText = "INHALA"; // Agrega el texto 'INHALA' en la fase de crecimiento
+          breathText.innerText = "INHALA"; // Agregar texto 'INHALA' en la fase de crecimiento
         } else {
-          // Cambia a la fase de espera
+          // Cambiar a la fase de espera
           fase = "espera";
-          breathText.innerText = "MANTÉN"; // Agrega el texto 'MANTÉN' en la fase de espera
+          breathText.innerText = "MANTÉN"; // Agregar texto 'MANTÉN' en la fase de espera
           setTimeout(() => {
-            fase = "decrecimiento"; // Después de 4 segundos, cambia a la fase de decrecimiento
+            fase = "decrecimiento"; // Después de 4 segundos, cambiar a la fase de decrecimiento
           }, 4000);
         }
       } else if (fase === "decrecimiento") {
@@ -121,28 +128,26 @@ document.addEventListener("DOMContentLoaded", () => {
         if (porcentaje > 30) {
           circuloTurquesa.style.width = `${porcentaje}%`;
           circuloTurquesa.style.height = `${porcentaje}%`;
-          porcentaje -= 2; // Ajusta la velocidad de decrecimiento
+          porcentaje -= 1.5; // Ajustar velocidad de decrecimiento
           const fontSize = `calc(${3 + porcentaje / 15}vw)`;
           breathText.style.cssText = `font-size: ${fontSize}; color: var(--color-blanco); transition: font-size 1s ease-in-out;`;
-          breathText.innerText = "EXHALA"; // Agrega el texto 'EXHALA' en la fase de decrecimiento
+          breathText.innerText = "EXHALA"; // Agregar texto 'EXHALA' en la fase de decrecimiento
         } else {
-          // Cambia a la fase de espera
+          // Cambiar a la fase de espera
           fase = "espera";
-          breathText.innerText = "MANTÉN"; // Agrega el texto 'MANTÉN' en la fase de espera
+          breathText.innerText = "MANTÉN"; // Agregar texto 'MANTÉN' en la fase de espera
           setTimeout(() => {
-            fase = "crecimiento"; // Después de 4 segundos, cambia a la fase de crecimiento
+            fase = "crecimiento"; // Después de 6 segundos, cambiar a la fase de crecimiento
           }, 4000);
         }
       }
-    }, 150); // El intervalo se ejecuta aproximadamente cada 150 milisegundos
+    }, 150); // Intervalo ejecutado cada 150 milisegundos
   }
 
   function mostrarFelicitacion() {
-    // Detener cualquier animación o acción en curso
+    // Mostrar mensaje de felicitación al finalizar la sesión
     clearInterval(animacionInterval); // Detener la animación
     circuloTurquesa.innerHTML = ""; // Limpiar contenido
-
-    // Reemplazar el contenido del contador con el mensaje de felicitación
     contador.innerText = "¡Felicidades!";
   }
 });
